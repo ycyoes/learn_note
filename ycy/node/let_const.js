2627d65ee16686e6183022bdd8204ad6aaad01cb
ycyoes <ycyoes@163.com>
2020-05-26 16:22:47 +0800

    块内变量

--- ycy/node/let_const.js
+++ ycy/node/let_const.js
@@ -55,11 +55,27 @@ typeof x; // ReferenceError
 // 作为比较，如果一个变量根本没有被声明，使用typeof反而不会报错。
 typeof undeclared_variable // "undefined"
 
+var tmp = new Date();
+function f() {
+    console.log(tmp);
+    if(false) {
+        var tmp = 'hello world';
+    }
+}
 
+f();
+// 上面代码的原意是，if代码块的外部使用外层的tmp变量，内部使用内层的tmp变量。但是，函数f执行后，输出结果为undefined，原因在于变量提升，
+// 导致内层的tmp变量覆盖了外层的tmp变量。
 
+// 用来计数的循环变量泄露为全局变量
+var s = 'hello';
 
+for (var i = 0; i < s.length; i++) {
+  console.log(s[i]);
+}
 
-
+console.log(i); // 5
+// 变量i只用来控制循环，但是循环结束后，它并没有消失，泄露成了全局变量
 
 
 
