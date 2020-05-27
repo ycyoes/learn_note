5272461b2e8ad512bf56fff253eef52a876e97d3
ycyoes <ycyoes@163.com>
2020-05-27 21:10:36 +0800

    等号右边的值不是对象或数组，就先将其转为对象

--- ycy/node/destructuring.js
+++ ycy/node/destructuring.js
@@ -48,6 +48,9 @@ console.log(s)
 console.log(s === Number.prototype.toString)
 console.log(Number.prototype.toString)
 
-
-
+// 解构赋值的规则是，只要等号右边的值不是对象或数组，就先将其转为对象。由于undefined和null无法转为对象，所以对它们进行解构赋值，都会报错。
+// let { prop: x2 } = undefined; // TypeError
+// let { prop: y2 } = null; // TypeError
+// console.log(x2);
+// console.log(y2)
 
