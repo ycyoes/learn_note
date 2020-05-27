5206b293dfec3b14a88433bba16e1e0081f60c27
ycyoes <ycyoes@163.com>
2020-05-28 00:02:34 +0800

    直接调用函数与变量的不同

--- ycy/node/generator.js
+++ ycy/node/generator.js
@@ -10,3 +10,19 @@ console.log(hw.next());
 console.log(hw.next());
 console.log(hw.next());
 console.log(hw.next());
+
+function* gen() {
+    yield 123 + 456;
+    yield 'test';
+
+    return 'gen';
+}
+
+let gen1 = gen();
+console.log(gen().next());
+console.log(gen().next());
+console.log(gen().next);
+
+console.log(gen1.next())
+console.log(gen1.next())
+console.log(gen1.next())
