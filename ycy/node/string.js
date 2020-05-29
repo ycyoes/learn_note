881375ec4acd5f66c0fdabb4f4d0ee5b2afca7a9
ycyoes <ycyoes@163.com>
2020-05-29 17:37:15 +0800

    字符串的遍历器接口

--- ycy/node/string.js
+++ ycy/node/string.js
@@ -1,1 +1,31 @@
-console.log("\u0061")
+// 字符的 Unicode 表示法
+console.log("\u0061");
+console.log("\uD842\uDFB7");
+console.log("\u20BB7");
+console.log("\u{20BB7}")
+console.log("\u{41}\u{42}\u{43}")
+console.log('\u{1F680}' === '\uD83D\uDE80')
+
+console.log('\u{7A}' === 'z')
+
+// 字符串的遍历器接口
+for (let codePoint of 'foo') {
+    console.log(codePoint)
+}
+
+let text = String.fromCodePoint(0x20BB7);
+
+for (let i = 0; i < text.length; i++) {
+  console.log(text[i]);
+}
+console.log('text: ', text)
+
+for (let i of text) {
+    console.log(i);
+}
+
+
+
+
+
+
