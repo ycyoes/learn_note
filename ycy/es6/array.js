f2ccaaea1f59775feb8dab133d5b24a539a2364a
ycyoes <ycyoes@163.com>
2020-06-08 21:57:18 +0800

    遍历数组

--- ycy/es6/array.js
+++ ycy/es6/array.js
@@ -190,7 +190,20 @@ for (let index of ['a', 'b'].keys()) {
   console.log(index);
 }
 
+for (let val of ['a', 'b'].values()) {
+  console.log(val);
+}
 
+for (let [index, elem] of ['a', 'b'].entries()) {
+  console.log(index, elem);
+}
+
+// 如果不使用for...of循环，可以手动调用遍历器对象的next方法，进行遍历
+let letter = ['a', 'b', 'c'];
+let entries = letter.entries();
+console.log(entries.next().value); // [0, 'a']
+console.log(entries.next().value); // [1, 'b']
+console.log(entries.next().value); // [2, 'c']
 
 
 
