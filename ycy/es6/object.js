a7677a1f41013e7a7c0b1279ac7f7c3c8efea7ef
ycyoes <ycyoes@163.com>
2020-06-09 08:10:18 +0800

    CommonJS 模块输出一组变量

--- ycy/es6/object.js
+++ ycy/es6/object.js
@@ -15,6 +15,52 @@ function f(x, y) {
 
   log(f(1, 2))
 
+  let birth = '2000/01/01';
+
+const Person = {
+
+  name: '张三',
+
+  //等同于birth: birth
+  birth,
+
+  // 等同于hello: function ()...
+  hello() { console.log('我的名字是', this.name); }
+
+};
+function getPoint() {
+    const x = 1;
+    const y = 10;
+    return {x, y};
+  }
+
+  log(getPoint())
+
+  let ms = {};
+
+function getItem (key) {
+  return key in ms ? ms[key] : null;
+}
+
+function setItem (key, value) {
+  ms[key] = value;
+}
+
+function clear () {
+  ms = {};
+}
+
+module.exports = { getItem, setItem, clear };
+// 等同于
+module.exports = {
+  getItem: getItem,
+  setItem: setItem,
+  clear: clear
+};
+
+
+
+
 
 
 
