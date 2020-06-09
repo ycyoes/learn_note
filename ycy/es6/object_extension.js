7c5bbb91854719e4c1a22fdbb17aa3b356962b4f
ycyoes <ycyoes@163.com>
2020-06-09 15:47:30 +0800

    Object.is

--- ycy/es6/object_extension.js
+++ ycy/es6/object_extension.js
@@ -1,2 +1,28 @@
 const {log} = console;
 // Object.is()用来比较两个值是否严格相等，与严格比较运算符（===）的行为基本一致
+log(+0 === -0)
+log(Object.is('foo', 'foo'))
+log(Object.is({}, {}))
+log(NaN === NaN)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
