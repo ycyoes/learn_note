f0839be23d6754991056f79bc4bed4ab85acfd91
ycyoes <ycyoes@163.com>
2020-06-11 23:25:52 +0800

    Set 结构的实例默认可遍历，它的默认遍历器生成函数就是它的values方法

--- ycy/es6/setmap.js
+++ ycy/es6/setmap.js
@@ -38,6 +38,55 @@ log(set4.size)
 set4.add({});
 log(set4.size )
 
+const items1 = new Set([1, 2, 3, 4, 5]);
+const array1 = Array.from(items1);
+log(array1)
+
+function dedupe(array) {
+    return Array.from(new Set(array));
+  }
+  
+  log(dedupe([1, 1, 2, 3]))
+
+  let set5 = new Set(['red', 'green', 'blue']);
+
+  for (let item of set5.keys()) {
+    console.log(item);
+  }
+
+  for (let item of set5.values()) {
+    console.log(item);
+  }
+
+  for (let item of set5.entries()) {
+    console.log(item);
+  }
+
+  // 由于 Set 结构没有键名，只有键值（或者说键名和键值是同一个值），所以keys方法和values方法的行为完全一致
+
+  // Set 结构的实例默认可遍历，它的默认遍历器生成函数就是它的values方法
+  log(Set.prototype[Symbol.iterator] === Set.prototype.values)
+  for (let x of set5) {
+    console.log(x);
+  }
+log(Symbol.iterator)
+log(Set.prototype[Symbol.iterator])
+log(Set.prototype.values)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
