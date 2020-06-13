09314f4a7a05d1562a662054fad065dfeac3a839
ycyoes <ycyoes@163.com>
2020-06-13 11:43:32 +0800

    Proxy

---
+++ ycy/es6/proxy.js
@@ -0,0 +1,27 @@
+const {log} = console;
+var obj = new Proxy({}, {
+    get: function (target, propKey, receiver) {
+      console.log(`getting ${propKey}!`);
+      return Reflect.get(target, propKey, receiver);
+    },
+    set: function (target, propKey, value, receiver) {
+      console.log(`setting ${propKey}!`);
+      return Reflect.set(target, propKey, value, receiver);
+    }
+  });
+
+  obj.count = 1
+  //  setting count!
+  ++obj.count
+//  getting count!
+//  setting count!
+//  2
+  log(obj)
+
+
+
+
+
+
+
+
