15369ad16602d5a285517492b9fe7e09a46c001c
ycyoes <ycyoes@163.com>
2020-06-24 07:20:25 +0800

    yield

--- ycy/es6/generator.js
+++ ycy/es6/generator.js
@@ -408,6 +408,133 @@ function* genFuncWithReturn() {
   
   log([...logReturned(genFuncWithReturn())])
 
+  function* iterTree(tree) {
+    if (Array.isArray(tree)) {
+      for(let i=0; i < tree.length; i++) {
+        yield* iterTree(tree[i]);
+      }
+    } else {
+      yield tree;
+    }
+  }
+  
+  const tree = [ 'a', ['b', 'c'], ['d', 'e'] ];
+  
+  for(let x of iterTree(tree)) {
+    console.log(x);
+  }
+
+  // 下面是二叉树的构造函数，
+// 三个参数分别是左树、当前节点和右树
+function Tree(left, label, right) {
+    this.left = left;
+    this.label = label;
+    this.right = right;
+  }
+  
+  // 下面是中序（inorder）遍历函数。
+  // 由于返回的是一个遍历器，所以要用generator函数。
+  // 函数体内采用递归算法，所以左树和右树要用yield*遍历
+  function* inorder(t) {
+    if (t) {
+      yield* inorder(t.left);
+      yield t.label;
+      yield* inorder(t.right);
+    }
+  }
+  
+  // 下面生成二叉树
+  function make(array) {
+    // 判断是否为叶节点
+    if (array.length == 1) return new Tree(null, array[0], null);
+    return new Tree(make(array[0]), array[1], make(array[2]));
+  }
+  let tree1 = make([[['a'], 'b', ['c']], 'd', [['e'], 'f', ['g']]]);
+
+  // 遍历二叉树
+var result = [];
+for (let node of inorder(tree1)) {
+  result.push(node);
+}
+
+log(result)
+
+function* F() {
+    this.a = 1;
+    yield this.b = 2;
+    yield this.c = 3;
+  }
+  var obj = {};
+  var f = F.call(obj);
+  log(f.next())
+  log(f.next())
+  log(f.next())
+
+  log(obj.a)
+  log(obj.b)
+  log(obj.c)
+
+  var clock = function* () {
+    while (true) {
+      console.log('Tick!');
+      yield;
+      console.log('Tock!');
+      yield;
+    }
+  };
+
+log(clock().next())
+log(clock().next())
+log(clock().next())
+log(clock().next())
+log(clock().next())
+
+let clo = clock()
+log(clo.next())
+log(clo.next())
+log(clo.next())
+
+function* gen6() {
+    yield 1;
+    return 2;
+  }
+  
+  let g6 = gen6();
+  
+  console.log(
+    g6.next().value,
+    g6.next().value,
+  );
+
+  function* iterEntries(obj) {
+    let keys = Object.keys(obj);
+    for (let i=0; i < keys.length; i++) {
+      let key = keys[i];
+      yield [key, obj[key]];
+    }
+  }
+  
+  let myObj = { foo: 3, bar: 7 };
+  
+  for (let [key, value] of iterEntries(myObj)) {
+    console.log(key, value);
+  }
+
+  function* makeSimpleGenerator(array){
+    var nextIndex = 0;
+  
+    while(nextIndex < array.length){
+      yield array[nextIndex++];
+    }
+  }
+  
+  var gen = makeSimpleGenerator(['yo', 'ya']);
+  
+log(  gen.next().value)
+log(  gen.next().value)
+log(  gen.next().value)
+
+
 
 
 
